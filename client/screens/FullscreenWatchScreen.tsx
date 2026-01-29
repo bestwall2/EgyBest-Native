5b9014186df70cf514340d759368e652c8b9f121
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2026-01-29 03:56:00 +0100

    Add injectedJavaScript to prevent popups

--- client/screens/FullscreenWatchScreen.tsx
+++ client/screens/FullscreenWatchScreen.tsx
@@ -8,6 +8,7 @@ import * as ScreenOrientation from "expo-screen-orientation";
 import { StatusBar, setStatusBarHidden } from "expo-status-bar";
 import { RootStackParamList } from "@/navigation/RootStackNavigator";
 
+
 const injectedJavaScript = `
 (function() {
   // prevent popups
